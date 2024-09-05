d18773600bd950c178076ccb5791081e018c1ad7
Hossein Rezaei <rezaeig22@gmail.com>
2024-09-05 19:33:55 +0330

    Set up react router

--- client/index.js
+++ client/index.js
@@ -4,6 +4,10 @@ import ReactDOM from "react-dom";
 import ApolloClient, { createNetworkInterface } from "apollo-client";
 import { ApolloProvider } from "react-apollo";
 
+import { Router, Route, hashHistory, IndexRoute } from "react-router";
+import AppLayout from "./components/Layout/AppLayout";
+import HomePage from "./components/pages/HomePage";
+
 const networkInterface = createNetworkInterface({
   uri: "/graphql",
   opts: { credentials: "same-origin" }
@@ -17,7 +21,11 @@ const client = new ApolloClient({
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <div>Auth Starter</div>
+      <Router history={hashHistory}>
+        <Route path="/" component={AppLayout}>
+          <IndexRoute component={HomePage} />
+        </Route>
+      </Router>
     </ApolloProvider>
   );
 };
