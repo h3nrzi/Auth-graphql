f1c942bfce575bdfc7b893a706361c71560f4834
Hossein Rezaei <rezaeig22@gmail.com>
2024-09-05 19:20:18 +0330

    Set up apollo client & provider

--- client/index.js
+++ client/index.js
@@ -1,8 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import ApolloClient, { createNetworkInterface } from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+
+const networkInterface = createNetworkInterface({
+  uri: "/graphql",
+  opts: { credentials: "same-origin" }
+});
+
+const client = new ApolloClient({
+  dataIdFromObject: (obj) => obj.id,
+  networkInterface
+});
+
 const Root = () => {
-  return <div>Auth Starter</div>;
+  return (
+    <ApolloProvider client={client}>
+      <div>Auth Starter</div>
+    </ApolloProvider>
+  );
 };
 
 ReactDOM.render(<Root />, document.querySelector("#root"));
